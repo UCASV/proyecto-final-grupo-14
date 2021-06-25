ac81bb951f2778e6a0884ae240e2552c4338bbaa
aligr <00015720@uca.edu.sv>
2021-06-24 21:51:44 -0600

    ADD: COVIDVACCSYSTEM FILES - 00015720

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Windows.Forms;
+using COVIDVACCSYSTEM.View;
+
+namespace COVIDVACCSYSTEM
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ManagerLogIn());
+            Application.Run(new MainWindow());
+        }
+    }
+}
