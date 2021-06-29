a564d9ebc8022493b02450a5984d1e41cdf79eda
cyfer2410 <00014220@uca.edu.sv>
2021-06-28 18:30:16 -0600

    Appointment generator repared

--- POO/View/AppointmentGenerator.cs
+++ POO/View/AppointmentGenerator.cs
@@ -22,79 +22,93 @@ namespace COVIDVACCSYSTEM.View
 
 
         private void GenerateButton_Click(object sender, EventArgs e)
-        
+
         {
             var db = new COVIDVACCDBContext.COVIDVACCDBContext();
-            var listUsers = db.VaccinationAppointments
+            var listVacc = db.VaccinationAppointments
+                .ToList();
+            var listCitizen = db.Citizens
                 .ToList();
 
-            var result = listUsers.Where(
+            var result = listVacc.Where(
                 u => u.CitizenId.Equals(DUITB.Text)
             ).ToList();
 
+            var resultCit = listCitizen.Where(
+                u => u.Dui.Equals(DUITB.Text)
+            ).ToList();
 
-            if (result.Count == 0)
+            if (resultCit.Count == 0)
             {
-
-                DateTime finish = new DateTime(2021, 10, 1);
-                Random gen = new Random();
-
-                int range = (finish - DateTime.Today).Days;
-                DateTime DateVaccine = DateTime.Today.AddDays(gen.Next(range));
-                DateTime newDateVaccine = DateVaccine.AddDays(42);
-
-                Random random = new Random();
-                TimeSpan start = TimeSpan.FromHours(7);
-                TimeSpan end = TimeSpan.FromHours(11);
-                int maxMinutes = (int)((end - start).TotalMinutes);
-
-                   int minutes = random.Next(maxMinutes);
-                   TimeSpan time = start.Add(TimeSpan.FromMinutes(minutes));
-
-
-                   Random rnd = new Random();
-                   int cabinid = rnd.Next(1, 42);
-
-
-
-
-               VaccinationAppointment Firstapp = new VaccinationAppointment
-                {
-
-                  AppDate = DateVaccine,
-                  AppTime = time,
-                  CabinId = cabinid,
-                  CitizenId = DUITB.Text,
-
-
-                };
-               VaccApp.Create(Firstapp);
-               VaccinationAppointment Secondapp = new VaccinationAppointment
-               {
-
-                   AppDate = newDateVaccine,
-                   AppTime = time,
-                   CabinId = cabinid,
-                   CitizenId = DUITB.Text,
-
-               };
-               VaccApp.Create(Secondapp);
-
-               MessageBox.Show("Cita para primera y segunda dosis creada con éxito. Ve a Seguimiento de Cita para más información.", "CITA", 
-                   MessageBoxButtons.OK, MessageBoxIcon.Information);
-
+                MessageBox.Show("El dui ingresado, no existe", "Vacunacion",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
-                MessageBox.Show("Ya tiene una cita vigente", "Vacunacion",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
 
+                if (result.Count == 0)
+                {
+
+                    DateTime finish = new DateTime(2021, 10, 1);
+                    Random gen = new Random();
+
+                    int range = (finish - DateTime.Today).Days;
+                    DateTime DateVaccine = DateTime.Today.AddDays(gen.Next(range));
+                    DateTime newDateVaccine = DateVaccine.AddDays(42);
+
+                    Random random = new Random();
+                    TimeSpan start = TimeSpan.FromHours(7);
+                    TimeSpan end = TimeSpan.FromHours(11);
+                    int maxMinutes = (int) ((end - start).TotalMinutes);
+
+                    int minutes = random.Next(maxMinutes);
+                    TimeSpan time = start.Add(TimeSpan.FromMinutes(minutes));
+
+
+                    Random rnd = new Random();
+                    int cabinid = rnd.Next(1, 42);
+
+
+
+
+                    VaccinationAppointment Firstapp = new VaccinationAppointment
+                    {
+
+                        AppDate = DateVaccine,
+                        AppTime = time,
+                        CabinId = cabinid,
+                        CitizenId = DUITB.Text,
+
+
+                    };
+                    VaccApp.Create(Firstapp);
+                    VaccinationAppointment Secondapp = new VaccinationAppointment
+                    {
+
+                        AppDate = newDateVaccine,
+                        AppTime = time,
+                        CabinId = cabinid,
+                        CitizenId = DUITB.Text,
+
+                    };
+                    VaccApp.Create(Secondapp);
+
+                    MessageBox.Show(
+                        "Cita para primera y segunda dosis creada con éxito. Ve a Seguimiento de Cita para más información.",
+                        "CITA",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else
+                {
+                    MessageBox.Show("Ya tiene una cita vigente", "Vacunacion",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
             }
+        }
 
 
-
-
-          }
-      }
-  } 
+    }
+} 
 
--- POO/View/Statistics.Designer.cs
+++ POO/View/Statistics.Designer.cs
@@ -40,24 +40,25 @@ namespace COVIDVACCSYSTEM.View
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 24F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.SlateGray;
-            this.label1.Location = new System.Drawing.Point(239, 9);
+            this.label1.Location = new System.Drawing.Point(269, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(494, 53);
+            this.label1.Size = new System.Drawing.Size(556, 66);
             this.label1.TabIndex = 0;
             this.label1.Text = "ESTADISTICAS";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Statistics
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (218)))), ((int) (((byte) (218)))), ((int) (((byte) (218)))));
             this.BackgroundImage = ((System.Drawing.Image) (resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(936, 650);
+            this.ClientSize = new System.Drawing.Size(1053, 812);
             this.Controls.Add(this.label1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Statistics";
